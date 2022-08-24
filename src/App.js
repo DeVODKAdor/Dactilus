7b39edae884155f512281f95f431672aa6fa1438
Davi <daviagrm@gmail.com>
2022-08-24 18:16:27 -0400

    new file:   .vscode/launch.json 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   public/manifest.json 	modified:   src/App.js 	new file:   src/components/webcamDisplay.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import WebcamDisplay from './components/webcamDisplay';
 
 function App() {
   return (
     <div className="App">
-      
+      <WebcamDisplay/>
     </div>
   );
 }
---
+++ src/components/webcamDisplay.jsx
@@ -0,0 +1,15 @@
+import * as handsMediapipe from '@mediapipe/hands'
+import * as cameraUtils from '@mediapipe/camera_utils'
+import * as drawingUtils from '@mediapipe/drawing_utils'
+import React, { createContext, useContext } from 'react';
+import { render } from '@testing-library/react';
+
+class WebcamDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    
+}
+
+export default WebcamDisplay;
