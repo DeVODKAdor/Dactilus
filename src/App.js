7fb8b64e0199570746fdcede92a53bcf0d59f89a
Davi <daviagrm@gmail.com>
2022-08-24 20:37:18 -0400

    modified:   public/index.html 	modified:   src/App.js 	modified:   src/components/webcamDisplay.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import WebcamDisplay from './components/webcamDisplay';
+import React from "react";
+import "./App.css";
+import WebcamDisplay from "./components/webcamDisplay";
 
 function App() {
   return (
     <div className="App">
-      <WebcamDisplay/>
+      <WebcamDisplay />
     </div>
   );
 }
