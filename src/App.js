37ed14b0a9a91c40e3e7947023f01a3bfdb78c01
Davi <daviagrm@gmail.com>
2022-08-30 15:01:59 -0400

    #	new file:   .firebaserc     #	deleted:    .vscode/launch.json     #	modified:   README.md     #	new file:   firebase.json     #	modified:   package-lock.json     #	modified:   package.json     #	modified:   public/index.html     #	modified:   src/App.js     #	deleted:    src/components/webcamDisplay.jsx     #	new file:   src/model/group1-shard1of1.bin     #	new file:   src/model/model.json     #	new file:   storage.rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
-import WebcamDisplay from "./components/webcamDisplay";
+import * as tf from '@tensorflow/tfjs'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIgv41dwonLQHn_H3oblp8Nicpm_nRggs",
+  authDomain: "dactilus-12bc4.firebaseapp.com",
+  projectId: "dactilus-12bc4",
+  storageBucket: "dactilus-12bc4.appspot.com",
+  messagingSenderId: "474280065419",
+  appId: "1:474280065419:web:3e9890d9bb67b3acd6d78e",
+  measurementId: "G-3LDHLXJ2EP"
+};
+const app = initializeApp(firebaseConfig);
+
 
 function App() {
   return (
     <div className="App">
-      <WebcamDisplay />
     </div>
   );
 }
