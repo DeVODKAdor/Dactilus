b9e18fb0db4a32ff836fddfcaa975c06d122fc41
Davi <daviagrm@gmail.com>
2022-09-17 15:08:49 -0400

    #	deleted:    .firebaserc     #	deleted:    firebase.json     #	modified:   src/App.js     #	deleted:    storage.rules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "./App.css";
-import * as tf from '@tensorflow/tfjs'
-import { initializeApp } from "firebase/app";
-import { getStorage, ref } from "firebase/storage";
 import WebcamDisplay from "./components/webcamDisplay";
 
 function App() {
