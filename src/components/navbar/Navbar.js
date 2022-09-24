36c08e413f0d42880019533960c601ef97063ffa
Davi <daviagrm@gmail.com>
2022-09-24 19:31:16 -0400

    #	new file:   public/dactiluslogo.ico     #	modified:   public/index.html     #	deleted:    src/assets/dactiluslogo.png     #	new file:   src/assets/fonts/poppins/Poppins-Regular.ttf     #	new file:   src/assets/images/arrow.png     #	new file:   src/assets/images/dactiluslogo.png     #	modified:   src/components/navbar/Navbar.css     #	modified:   src/components/navbar/Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import arrow from "../../assets/images/arrow.png"
+import logo from "../../assets/images/dactiluslogo.png";
 
 function Navbar() {
   return (
     <div>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to="/" className="navbar-logo">
-                    <img src="../../assets/dactiluslogo.png" alt='dactilus logo'/>
-                </Link>
-            </div>
-        </nav>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <img src={logo} alt="dactilus logo" className="logo" />
+          </Link>
+          <Link to="/" className="navbar-home">
+            Home
+          </Link>
+          <Link to="/dicionario" className="navbar-dicionario">
+            Dicionário
+            <img src={arrow} className="arrow"></img>
+          </Link>
+          <Link className="navbar-traduzir">Traduzir</Link>
+          <Link to="/chat" className="navbar-chat">Chat</Link>
+          <Link to="/login" className="navbar-login">Login</Link>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Navbar;
