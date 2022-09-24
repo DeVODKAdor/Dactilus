c544bf966fe096295a6cd89d15711dd28fbdf9e3
Davi <daviagrm@gmail.com>
2022-09-24 12:26:47 -0400

    #	modified:   src/App.js     #	renamed:    src/assets/dactilus-logo.png -> src/assets/dactiluslogo.png     #	renamed:    src/components/Navbar.css -> src/components/navbar/Navbar.css     #	renamed:    src/components/Navbar.js -> src/components/navbar/Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <>
+    <div>
         <nav className='navbar'>
             <div className='navbar-container'>
                 <Link to="/" className="navbar-logo">
-                    <img src="../assets/dactilus-logo.png"></img>
+                    <img src="../../assets/dactiluslogo.png" alt='dactilus logo'/>
                 </Link>
             </div>
         </nav>
-    </>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
