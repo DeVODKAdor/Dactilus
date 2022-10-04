795bf9cf76a9330b306594bba756213dbd8e9cf9
Davi <daviagrm@gmail.com>
2022-10-03 23:04:54 -0400

    #	new file:   src/components/webcam/Mediapipe.js     #	modified:   src/pages/traducao-libras/TraducaoLibras.js

---
+++ src/components/webcam/Mediapipe.js
@@ -0,0 +1,109 @@
+import * as mpHands from "@mediapipe/hands";
+import * as mpCamera from "@mediapipe/camera_utils";
+import * as drawing from "@mediapipe/drawing_utils";
+import Webcam from "react-webcam";
+import { useEffect, useRef } from "react";
+
+const Mediapipe = () => {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const onResults = (results) => {
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    // Set canvas width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        drawing.drawConnectors(canvasCtx, landmarks, mpHands.HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5,
+        });
+        drawing.drawLandmarks(canvasCtx, landmarks, {
+          color: "#FF0000",
+          lineWidth: 2,
+        });
+      }
+    }
+    canvasCtx.restore();
+  };
+
+  useEffect(() => {
+    const hands = new mpHands.Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+    hands.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      let camera = new mpCamera.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await hands.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start()
+    }
+  }, []);
+
+  return (
+    <>
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        className="output_canvas"
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+    </>
+  );
+};
+
+export default Mediapipe;
--- src/pages/traducao-libras/TraducaoLibras.js
+++ src/pages/traducao-libras/TraducaoLibras.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./TraducaoLibras.css"
 import Navbar from "../../components/navbar/Navbar";
 import WebcamDisplay from "../../components/webcam/Webcam";
+import Mediapipe from "../../components/webcam/Mediapipe"
 
 function TraducaoLibras() {
   const texto =
@@ -11,7 +12,7 @@ function TraducaoLibras() {
       <Navbar />
       <p className="aviso">{texto}</p>
       <div className="display">
-        <WebcamDisplay />
+        <Mediapipe />
       </div>
     </>
   );
